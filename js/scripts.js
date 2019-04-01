c8517ce5154d6ba824c840459854a883e0bf30ff
Alvin Peter <nivlapeter.0549@gmail.com>
2019-04-01 12:22:46 +0300

    add set of questions

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,19 @@ $(document).ready(function() {
  $("form#quiz").submit(function(event){
     event.preventDefault();
     $(#display).text(answer);
-    var Q1 = $("input:radio[name=Q1]:checked").val();
-    var Q2 = $("input:radio[name=Q2]:checked").val();
-    var Q3 = $("input:radio[name=Q3]:checked").val();
-    var Q4 = $("input:radio[name=Q4]:checked").val();
-    var Q5 = $("input:radio[name=Q5]:checked").val();
-    var Q6 = $("input:radio[name=Q6]:checked").val();
+    var Q1 = $(parseInt("input:radio[name=Q1]:checked")).val();
+    var Q2 = $(parseInt("input:radio[name=Q2]:checked")).val();
+    var Q3 = $(parseInt("input:radio[name=Q3]:checked")).val();
+    var Q4 = $(parseInt("input:radio[name=Q4]:checked")).val();
+    var Q5 = $(parseInt("input:radio[name=Q5]:checked")).val();
+    var Q6 = $(parseInt("input:radio[name=Q6]:checked")).val();
   
-    var answer = parseInt(Q1)+parseInt(Q2)+parseInt(Q3)+parseInt(Q4)+parseInt(Q5)+parseInt(Q6);
+    var answer = (Q1)+(Q2)+parseInt(Q3)+(Q4)+(Q5)+(Q6);
     $("#input").text("HI THERE! HERE'S WHAT YOU SCORED: " +answer+ "%");
        
        if (answer === 40){
            $(#display).text("pass");
-       }
+}
        else if (result >=40||<=70 ){
         $(#display).text("average")
        }
