3e6cecb9a34864931089aded480202a13313b60f
nivlapeter <48449873+nivlapeter@users.noreply.github.com>
2019-04-03 12:49:31 +0300

    ammend changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,58 @@
-
-$(document).ready(function() {
- $("form#quiz").submit(function(event){
-    event.preventDefault();
-    $(#display).text(answer);
-    var Q1 = $("input:radio[name=Q1]:checked").val();
-    var Q2 = $("input:radio[name=Q2]:checked").val();
-    var Q3 = $("input:radio[name=Q3]:checked").val();
-    var Q4 = $("input:radio[name=Q4]:checked").val();
-    var Q5 = $("input:radio[name=Q5]:checked").val();
-    var Q6 = $("input:radio[name=Q6]:checked").val();
+$(document).ready(function () {
+  $("#btn-submit").click(function () {
+    $("#quiz").fadeOut(function() {
+      
+      var Q1 = parseInt($("input:radio[name='Q1']:checked").val());
+      var Q2 = parseInt($("input:radio[name='Q2']:checked").val());
+      var Q3 = parseInt($("input:radio[name='Q3']:checked").val());
+      var Q4 = parseInt($("input:radio[name='Q4']:checked").val());
+      var Q5 = parseInt($("input:radio[name='Q5']:checked").val());
   
-    var answer = parseInt(Q1)+parseInt(Q2)+parseInt(Q3)+parseInt(Q4)+parseInt(Q5)+parseInt(Q6);
-    $("#input").text("HI THERE! HERE'S WHAT YOU SCORED: " +answer+ "%");
-       
-       if (answer === 40){
-           $(#display).text("pass");
-       }
-       else if (result >=40||<=70 ){
-        $(#display).text("average")
-       }
-       else if (result >=70||<=120)
-       $(#display).text("great") 
-       }
-       else{
-        $(#display).text("fail") 
-       }
-        
-    });
-  });
-   //==== user Interface ====//
-   $(document). ready(function(){
-       $("p").click(function(){
+      var results = parseInt(Q1 + Q2 + Q3 + Q4 + Q5);
 
-       });
-   });
+      if(results >= 80) {
+        $("#percentage").show(function() {
+          $(".score").text(results);
+        $(".condition").text("Passed Excellently!");
+        })
+        
+      }else if(results >= 50) {
+        $("#percentage").show(function() {
+          $(".score").text(results);
+        $(".condition").text("Passed fairly!");
+        })
+        
+      }else {
+        $("#percentage").show(function() {
+          $(".score").text(results);
+        $(".condition").text("You have failed! Retake the quiz!");
+        })
+        
+      }
+    
+    });
+
+
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
